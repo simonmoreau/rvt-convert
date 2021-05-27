6854e808c97a492a223a23b3b4e88fcf47302135
Simon Moreau <simon@bim42.com>
2021-05-27 22:26:58 +0200

    add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,23 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  value = 'World';
+  isIframe = false;
+  loggedIn = false;
+  userName: any;
+  loading: boolean;
+
+  constructor() {
+
+  }
+
+  login() {
+
+  }
+
+  logout() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,23 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatProgressBarModule} from '@angular/material/progress-bar';
+
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule],
+  declarations: [AppComponent, HomeComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatProgressBarModule,
+    AppRoutingModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
