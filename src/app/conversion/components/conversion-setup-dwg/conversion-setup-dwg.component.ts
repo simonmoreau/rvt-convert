5271a4d1e356d248702d32f25438b4983bb0a12d
Simon Moreau <simon@bim42.com>
2021-07-10 17:41:39 +0200

    set up DWG export settings

--- src/app/conversion/components/conversion-setup-dwg/conversion-setup-dwg.component.ts
+++ src/app/conversion/components/conversion-setup-dwg/conversion-setup-dwg.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ConversionSetupDwgComponent implements OnInit {
 
   panelOpenState = false;
+  export: boolean = true;
   
   constructor() { }
 
